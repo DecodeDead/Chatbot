75d630517a1a48a4a8bcbe6b3b978030eca9a07f
unknown <13158920ta@example.com>
2024-05-27 12:47:49 +0530

    changing port

--- fastapi-app/app/main.py
+++ fastapi-app/app/main.py
@@ -8,4 +8,4 @@ app.include_router(upload.router)
 app.include_router(chat.router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
