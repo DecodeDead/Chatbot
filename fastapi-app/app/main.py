7cfa1d9f533dd6a64474add245a63bb44baa1e26
unknown <13158920ta@example.com>
2024-05-28 21:12:05 +0530

    Trying a variable PORT

--- fastapi-app/app/main.py
+++ fastapi-app/app/main.py
@@ -3,12 +3,12 @@ from fastapi import FastAPI
 import uvicorn
 from app.routers import upload, chat
 
-
+PORT = "8000"
 app = FastAPI()
 app.include_router(upload.router)
 app.include_router(chat.router)
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
 
