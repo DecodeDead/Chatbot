3d3e82fd21e2924e0c8fed6ff95558a535b5b86e
unknown <13158920ta@example.com>
2024-05-28 21:09:56 +0530

    reverting earlier commit

--- fastapi-app/app/main.py
+++ fastapi-app/app/main.py
@@ -4,12 +4,11 @@ import uvicorn
 from app.routers import upload, chat
 
 
-port = process.env.PORT || 4000
 app = FastAPI()
 app.include_router(upload.router)
 app.include_router(chat.router)
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
